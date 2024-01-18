0c55371ffe9699530fcd730e8d679769c00663e8
adriansanchezlarrondo <123000464+adriansanchezlarrondo@users.noreply.github.com>
2024-01-18 10:15:49 -0800

    Add files via upload

---
+++ party.php
@@ -0,0 +1,92 @@
+<?php
+
+abstract class Party {
+    private $displayName;
+
+    function __construct($displayName) {
+        $this->displayName = $displayName;
+    }
+
+    public function getDisplayName() {
+        return $this->displayName;
+    }
+    
+}
+
+class Person extends Party {
+    private $firstName;
+    private $lastName;
+
+    function __construct($displayName, $firstName, $lastName) {
+        parent::__construct($displayName);
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+}
+
+class OrgUnit extends Party {
+    private $name;
+    private $employees = array();
+
+    function __construct($displayName, $name) {
+        parent::__construct($displayName);
+        $this->name = $name;
+    }
+
+    public function addEmployee(Person $employee) {
+        $this->employees[] = $employee;
+    }
+
+    public function getDisplayName() {
+        return $this->name;
+    }
+    
+    public function getEmployees() {
+        return $this->employees;
+    }
+    
+
+}
+
+class Company extends Party {
+    public $name;
+    private $units = array();
+
+    function __construct($displayName) {
+        parent::__construct($displayName);
+    }
+
+    public function addUnit(OrgUnit $unit) {
+        $this->units[] = $unit;
+    }
+
+    public function getDescription() {
+        foreach($this->units as $unit){
+            echo "\nUnit: " . $unit->getDisplayName();
+            
+            foreach($unit->getEmployees() as $employee) {
+                echo "\t\nEmployee: " . $employee->getDisplayName();
+            }
+        }
+    }
+}
+
+// Crear instancias
+$company = new Company("MyCompany");
+
+// Crear personas
+$person1 = new Person("Person One", "Person", "One");
+$person2 = new Person("Person One", "Person", "Two");
+
+// Crear OrgUnits
+$orgUnit1 = new OrgUnit("OrgUnit1", "Org1");
+$orgUnit2 = new OrgUnit("OrgUnit2", "Org2");
+
+$orgUnit1->addEmployee($person1);
+$orgUnit2->addEmployee($person2);
+
+$company->addUnit($orgUnit1);
+$company->addUnit($orgUnit2);
+
+// Mostrar información
+$company->getDescription();
